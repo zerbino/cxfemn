4ccb3272640f4612b304dda3b0e661ae3c0af415
Rambo21 <kevin.llopart@gmail.com>
2013-10-29 19:57:08 +0100

    Bugs examples commited

---
+++ SOAPValueSubstitutionClient/src/model/Personne.java
@@ -0,0 +1,87 @@
+
+package model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java pour personne complex type.
+ * 
+ * <p>Le fragment de schéma suivant indique le contenu attendu figurant dans cette classe.
+ * 
+ * <pre>
+ * &lt;complexType name="personne">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="nom" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="prenom" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "personne", propOrder = {
+    "nom",
+    "prenom"
+})
+public class Personne {
+
+    protected String nom;
+    protected String prenom;
+
+    /**
+     * Obtient la valeur de la propriété nom.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getNom() {
+        return nom;
+    }
+
+    /**
+     * Définit la valeur de la propriété nom.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setNom(String value) {
+        this.nom = value;
+    }
+
+    /**
+     * Obtient la valeur de la propriété prenom.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getPrenom() {
+        return prenom;
+    }
+
+    /**
+     * Définit la valeur de la propriété prenom.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setPrenom(String value) {
+        this.prenom = value;
+    }
+
+}
