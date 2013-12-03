41637adfe363e4b69b1fdacb58a63f87a11c51cc
zerbino <r2mart10@gmail.com>
2013-12-03 18:30:46 +0100

    commit arach

---
+++ workspace/EssaiFiltre/src/client/Personne.java
@@ -0,0 +1,36 @@
+package client;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Personne {
+	
+	private String nom;
+	
+	private String prenom;
+
+	
+
+	public Personne() {
+		super();
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+	
+
+}
