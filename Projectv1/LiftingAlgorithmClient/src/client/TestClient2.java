dc74b2a58a373e93c31b88f6e7606ab0515d34ae
zerbino <r2mart10@gmail.com>
2013-12-16 08:06:10 +0100

    commit du matin

--- Projectv1/LiftingAlgorithmClient/src/client/TestClient2.java
+++ Projectv1/LiftingAlgorithmClient/src/client/TestClient2.java
@@ -1,10 +1,16 @@
 package client;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import model.Personne;
 import model.Service;
 
 import org.apache.cxf.jaxrs.client.JAXRSClientFactory;
 
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
+import filter.ResponseFilter;
 import utile.UniformementRepresentable;
 
 /**
@@ -15,9 +21,10 @@ import utile.UniformementRepresentable;
 public class TestClient2 {
 
 	public static void main(String[] args) {
-		
+		List<Object> filtres = new LinkedList<>();
+		filtres.add(new ResponseFilter());
 		Service service = JAXRSClientFactory.create(
-				"http://localhost:8080/LiftingAlgorithm", Service.class);
+				"http://localhost:8080/LiftingAlgorithm", Service.class, filtres);
 		Personne etudiant = service.op();
 		System.out.println(etudiant.getClass().getSimpleName());
 		System.out.println(UniformementRepresentable.toString(
